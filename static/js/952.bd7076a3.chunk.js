0bdd764e9a46f3117af05941bc4562306beb1b22
vqhengr <40216897+vqhengr@users.noreply.github.com>
2024-12-21 07:02:31 +0000

    Updates

---
+++ static/js/952.bd7076a3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdance_curriculum=self.webpackChunkdance_curriculum||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.bd7076a3.chunk.js.map
